77ebad182aaac0aeb7480c67b6197137bbac47b6
LordBao666 <916900021@qq.com>
2026-06-14 10:27:31 +0800

    Add -n parameter for sort

--- TheC/ch05pointers/content/sortdemo/qsort.c
+++ TheC/ch05pointers/content/sortdemo/qsort.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-static void swap(char *v[], int i, int j) {
-    char *p = v[i];
+static void swap(void *v[], int i, int j) {
+    void *p = v[i];
     v[i] = v[j];
     v[j] = p;
 }
 
-void myQsort(char *v[], int left, int right) {
+void myQsort(void *v[], int left, int right, int (*comp)(void *, void *)) {
     if (left >= right) {
         return;
     }
@@ -20,15 +20,15 @@ void myQsort(char *v[], int left, int right) {
     int last = left, i = left + 1;
     while (i <= right) {
         // v[i]比基准v[left]小
-        if (strcmp(v[i], v[left]) < 0) {
+        if ((*comp)(v[i], v[left]) < 0) {
             swap(v, ++last, i);
         }
         i++;
     }
     // 将基准放到正确的位置
     swap(v, last, left);
-    myQsort(v, left, last - 1);
-    myQsort(v, last + 1, right);
+    myQsort(v, left, last - 1, comp);
+    myQsort(v, last + 1, right, comp);
 }
 
 // int main() {
@@ -38,7 +38,7 @@ void myQsort(char *v[], int left, int right) {
 //         "joker",
 //     };
 //     const int size = 3;
-//     myQsort(strS, 0, size - 1);
+//     myQsort((void**)strS, 0, size - 1,(int (*)(void*,void*))strcmp);
 //     for (int i = 0; i < size; i++) {
 //         printf("%s\n", strS[i]);
 //     }
--- TheC/ch05pointers/content/sortdemo/qsort.h
+++ TheC/ch05pointers/content/sortdemo/qsort.h
@@ -1,5 +1,5 @@
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
-//对字符串数组 v[left...right]进行快速排序
-void myQsort(char *v[], int left, int right);
+//对数组 v[left...right]进行快速排序,comp是用于比较的函数指针
+void myQsort(void *v[], int left, int right, int (*comp)(void *, void *));
 #endif
